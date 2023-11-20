02a9e4c00d2a213c154db8426147853adb4f8a18
Rachel McWhirter <rachel99mcwhirter@gmail.com>
2023-11-20 15:40:18 -0800

    Get login and signup to show up on home page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
   const login = () => {};
   return (
     <div className="login">
@@ -8,13 +10,13 @@ function Login() {
       <input
         placeholder="Username"
         onChange={(event) => {
-          setUser({ ...user, username: event.target.value });
+          setUsername(event.target.value);
         }}
       />
       <input
         placeholder="Password"
         onChange={(event) => {
-          setUser({ ...user, password: event.target.value });
+          setPassword(event.target.value);
         }}
       />
       <button onClick={login}>Login</button>
